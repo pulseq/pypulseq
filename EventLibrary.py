274697c81e71b2fc50aae2b23cac1fb5ac36d4b1
Stefan Kroboth <stefan.kroboth@uniklinik-freiburg.de>
2017-02-23 14:27:43 +0100

    EventLibrary.py now PEP8 compatible ... more or less

--- EventLibrary.py
+++ EventLibrary.py
@@ -6,17 +6,20 @@ Created on Thu Jun 23 13:37:10 2016
 """
 
 import collections
-import numpy as np
 import numpy.linalg as linalg
 
-class EventLibrary:
+# TODO:
+#   * Catch actual exceptions instead of all of them
+
+
+class EventLibrary(object):
     """
     EventLibrary   Maintain a list of events.
-    
-    The class is used by the Sequence class to store events of an MRI 
+
+    The class is used by the Sequence class to store events of an MRI
     sequence defined using the Pulseq file format.
        See http://pulseq.github.io/
-    
+
     Sequence Properties:
        keys - A list of event IDs
        data - A struct array with field 'array' to store data of varying
@@ -24,22 +27,21 @@ class EventLibrary:
        lengths - Corresponding lengths of the data arrays
        type - Type to distinguish events in the same class (e.g.
               trapezoids and arbitrary gradients)
-    
+
     Sequence Methods:
        find - Find an event in the library
        insert - Add a new event to the library
-    
+
     See also   mr.Sequence
-    
+
     Stefan Kroboth <stefan.kroboth@uniklinik-freiburg.de>
     Kelvin Layton <kelvin.layton@uniklinik-freiburg.de>
     """
-    
     def __init__(self):
-        self.keys    = dict() # TODO: List may suffice
-        self.data    = dict()
+        self.keys = dict()  # TODO: List may suffice
+        self.data = dict()
         self.lengths = dict()
-        self.type    = dict()
+        self.type = dict()
 
     def find(self, data):
         """
@@ -51,60 +53,54 @@ class EventLibrary:
         """
         # TODO: solve this better!
         try:
-            dataLength = len(data)
+            data_length = len(data)
         except:
-            try: 
-                dataLength = data.size
+            try:
+                data_length = data.size
             except:
-                dataLength = 1
+                data_length = 1
 
-        found = False;
-        id = None;
+        found = False
+        idx = None
 
         for ind in self.keys:
-            if (self.lengths[ind] == dataLength) and (type(self.data[ind]) == type(data)) and (linalg.norm(self.data[ind]-data)<1e-6):
-                id = self.keys[ind]
+            if (self.lengths[ind] == data_length) and \
+               (type(self.data[ind]) == type(data)) and \
+               (linalg.norm(self.data[ind]-data) < 1e-6):
+                idx = self.keys[ind]
                 found = True
                 break
 
         if not self.keys:
-            id = 1
+            idx = 1
         elif not found:
-            id = max(self.keys.keys())+1
+            idx = max(self.keys.keys())+1
 
-        out = collections.namedtuple('find', ['id', 'found']);
-        return out(id, found)
+        out = collections.namedtuple('find', ['id', 'found'])
+        return out(idx, found)
 
-
-
-    def insert(self, id, data, type=None):
+    def insert(self, idx, data, ttype=None):
         """
         Add event to library
 
         See also find
         """
 
-        self.keys[id] = id
-        self.data[id] = data
+        self.keys[idx] = idx
+        self.data[idx] = data
         # TODO: solve this better!
         try:
-            self.lengths[id] = len(data)
+            self.lengths[idx] = len(data)
         except:
             try:
-                self.lengths[id] = data.size
+                self.lengths[idx] = data.size
             except:
-                self.lengths[id] = 1
-        if type is not None:
-            self.type[id] = type
+                self.lengths[idx] = 1
+        if ttype is not None:
+            self.type[idx] = ttype
 
-    def getIDsOfType(self, type):
+    def get_ids_of_type(self, ttype):
         """
         Return all IDs with a given type
         """
-
-        return [k for (k,v) in self.type.iteritems() if v == type]
-
-
-
-        
-        
+        return [k for (k, v) in self.type.iteritems() if v == ttype]
