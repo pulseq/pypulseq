d42de0f7c13fec245fc9b3cd9c6749dbb5fe8f9d
Stefan Kroboth <stefan.kroboth@uniklinik-freiburg.de>
2017-02-28 13:57:20 +0100

    more pep8

--- writeSelectiveRf.py
+++ writeSelectiveRf.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+"""
+TODO
+@author: Stefan Kroboth
+"""
 
 from Sequence import Sequence
 import numpy as np
@@ -10,14 +15,14 @@ seq = Sequence()
 
 # Define FOV and resolution
 fov = 220e-3
-Nx = 256
-Ny = 256
+nx = 256
+ny = 256
 
 # Field of excitation
 foe = 200e-3
 
 # Diameter of target excitation pattern
-targetWidth = 22.5e-3
+target_width = 22.5e-3
 
 # Number of spiral turns
 n = 8
@@ -26,61 +31,61 @@ n = 8
 T = 8e-3
 
 # Define spiral k-space trajectory
-kMax = (2.0*n)/foe/2.0  # Units of 1/m (not rad/m)
-tk = np.linspace(0, T-seq.gradRasterTime, T/seq.gradRasterTime)
-kx = kMax*(1-tk/T)*np.cos(2*np.pi*n*tk/T)
-ky = kMax*(1-tk/T)*np.sin(2*np.pi*n*tk/T)
+k_max = (2.0*n)/foe/2.0  # Units of 1/m (not rad/m)
+tk = np.linspace(0, T-seq.grad_raster_time, T/seq.grad_raster_time)
+kx = k_max*(1-tk/T)*np.cos(2*np.pi*n*tk/T)
+ky = k_max*(1-tk/T)*np.sin(2*np.pi*n*tk/T)
 
 # Define RF pulse
-tr = np.arange(0, T-seq.rfRasterTime, seq.rfRasterTime)
+tr = np.arange(0, T-seq.rf_raster_time, seq.rf_raster_time)
 f = interp1d(tk, kx, kind='linear', fill_value='extrapolate')
 kxRf = f(tr)
 f = interp1d(tk, ky, kind='linear', fill_value='extrapolate')
 kyRf = f(tr)
-beta = 2.0*np.pi*kMax*targetWidth/2.0/np.sqrt(2)  # Gaussian width in k-space
+beta = 2.0*np.pi*k_max*target_width/2.0/np.sqrt(2)  # Gaussian width in k-space
 signal0 = np.exp(-beta**2*(1.0-tr/T)**2)*np.sqrt((2.0*np.pi*n*(1.0-tr/T))**2+1)
 signal = signal0*(1.0 + np.exp(-1j*2.0*np.pi*5e-2*(kxRf+kyRf)))
 
 # Add gradient ramps
-out = mr.addRamps([kx, ky], rf=signal)
+out = mr.add_ramps([kx, ky], rf=signal)
 kx = out[0]
 ky = out[1]
 signal = out[2]
 
-rf = mr.makeArbitraryRf(signal, 20.0*np.pi/180)
-gxRf = mr.makeArbitraryGrad('x', mr.traj2grad(kx))
-gyRf = mr.makeArbitraryGrad('y', mr.traj2grad(ky))
+rf = mr.make_arbitrary_rf(signal, 20.0*np.pi/180)
+gxRf = mr.make_arbitrary_grad('x', mr.traj2grad(kx))
+gyRf = mr.make_arbitrary_grad('y', mr.traj2grad(ky))
 
 # Define other gradients and ADC events
 deltak = 1.0/fov
-gx = mr.makeTrapezoid('x', flatArea=Nx*deltak, flatTime=6.4e-3)
-adc = mr.makeAdc(Nx, duration=gx.flatTime, delay=gx.riseTime)
-gxPre = mr.makeTrapezoid('x', area=-gx.area/2, duration=2e-3)
-phaseAreas = (np.arange(Ny)-Ny/2)*deltak
+gx = mr.make_trapezoid('x', flat_area=nx*deltak, flat_time=6.4e-3)
+adc = mr.make_adc(nx, duration=gx.flat_time, delay=gx.rise_time)
+gx_pre = mr.make_trapezoid('x', area=-gx.area/2, duration=2e-3)
+phase_areas = (np.arange(ny)-ny/2)*deltak
 
 # Refocusing pulse and spoiling gradients
-rf180 = mr.makeBlockPulse(np.pi, duration=1e-3, sliceThickness=5e-3)
+rf180 = mr.make_block_pulse(np.pi, duration=1e-3, slice_thickness=5e-3)
 gz = rf180.gz
-gzSpoil = mr.makeTrapezoid('z', area=gx.area, duration=2e-3)
+gzSpoil = mr.make_trapezoid('z', area=gx.area, duration=2e-3)
 
 # Calculate timing (TE=20ms, TR=500ms)
-delayTE1 = 20e-3/2.0 - mr.calcDuration(gzSpoil) - mr.calcDuration(rf180)/2.0
-delayTE2 = delayTE1 - mr.calcDuration(gxPre) - mr.calcDuration(gx)/2.0
-delayTR = 500e-3 - 20e-3 - mr.calcDuration(rf) - mr.calcDuration(gx)/2.0
+delay_TE1 = 20e-3/2.0 - mr.calc_duration(gzSpoil) - mr.calc_duration(rf180)/2.0
+delay_TE2 = delay_TE1 - mr.calc_duration(gx_pre) - mr.calc_duration(gx)/2.0
+delay_TR = 500e-3 - 20e-3 - mr.calc_duration(rf) - mr.calc_duration(gx)/2.0
 
 # Loop over phase encodes and define sequence blocks
-for i in range(Ny):
-    seq.addBlock([rf, gxRf, gyRf])
-    seq.addBlock(mr.makeDelay(delayTE1))
-    seq.addBlock(gzSpoil)
-    seq.addBlock([rf180, gz])
-    seq.addBlock(gzSpoil)
-    seq.addBlock(mr.makeDelay(delayTE2))
-    gyPre = mr.makeTrapezoid('y', area=phaseAreas[i], duration=2e-3)
-    seq.addBlock([gxPre, gyPre])
-    seq.addBlock([gx, adc])
-    seq.addBlock(mr.makeDelay(delayTR))
+for i in range(ny):
+    seq.add_block([rf, gxRf, gyRf])
+    seq.add_block(mr.make_delay(delay_TE1))
+    seq.add_block(gzSpoil)
+    seq.add_block([rf180, gz])
+    seq.add_block(gzSpoil)
+    seq.add_block(mr.make_delay(delay_TE2))
+    gyPre = mr.make_trapezoid('y', area=phase_areas[i], duration=2e-3)
+    seq.add_block([gx_pre, gyPre])
+    seq.add_block([gx, adc])
+    seq.add_block(mr.make_delay(delay_TR))
 
-seq.setDefinition('Scan_ID', 2068)
-seq.setDefinition('Recon_Mode', 1)
+seq.set_definition('Scan_ID', 2068)
+seq.set_definition('Recon_Mode', 1)
 seq.write('external.seq')
