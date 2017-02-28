d44b30677326c3b5e791146356c3d7ed61e3b1bd
Stefan Kroboth <stefan.kroboth@uniklinik-freiburg.de>
2017-02-28 13:43:56 +0100

    more PEP8

--- writeGradientEcho3D.py
+++ writeGradientEcho3D.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
 """
 TODO
 @author: Stefan Kroboth
@@ -6,88 +7,105 @@ TODO
 
 from Sequence import Sequence
 import numpy as np
-import matplotlib.pyplot as plt
 import mr
 
 # Define FOV
 fov = np.array((190e-3, 190e-3, 190e-3))
 
 # Define resolution
-Nx = 64
-Ny = Nx
-Nz = Nx
+nx = 64
+ny = nx
+nz = nx
 
 # Define other sequence parameters
-Tread = 3.2e-3
-Tpre = 3e-3
-riseTime = 400e-6
-Ndummy = 50
+t_read = 3.2e-3
+t_pre = 3e-3
+rise_time = 400e-6
+n_dummy = 50
 
 # Define system limits
-sys = mr.opts(maxGrad=20, gradUnit='mT/m', riseTime=riseTime, rfDeadTime=10e-6, adcDeadTime=10e-6)
+sys = mr.opts(max_grad=20, grad_unit='mT/m', rise_time=rise_time,
+              rf_dead_time=10e-6, adc_dead_time=10e-6)
 
 # Create a new sequence object
 seq = Sequence(system=sys)
 
 # Define other gradients and ADC events
-deltak  = 1.0/fov
-gx      = mr.makeTrapezoid('x', system=sys, flatArea=Nx*deltak[0], flatTime=Tread)
-gxPre   = mr.makeTrapezoid('x', system=sys, area=-gx.area/2, duration=Tpre)
-gxSpoil = mr.makeTrapezoid('x', system=sys, area=gx.area,    duration=Tpre)
-rf      = mr.makeBlockPulse(8*np.pi/180, system=sys, duration=0.2e-3) # So far only for timing calculation, will be overwritten later (not necessary, becaus duration is known -- just to keep it more consistent with the Matlab version (SK)
-areaY = (np.arange(Ny)-Ny/2.0)*deltak[1]
-areaZ = (np.arange(Nz)-Nz/2.0)*deltak[2]
+deltak = 1.0/fov
+gx = mr.make_trapezoid('x', system=sys, flat_area=nx*deltak[0],
+                       flat_time=t_read)
+gx_pre = mr.make_trapezoid('x', system=sys, area=-gx.area/2, duration=t_pre)
+gx_spoil = mr.make_trapezoid('x', system=sys, area=gx.area, duration=t_pre)
+# So far only for timing calculation, will be overwritten later (not necessary,
+# because duration is known -- just to keep it more consistent with the Matlab
+# version (SK):
+rf = mr.make_block_pulse(8*np.pi/180, system=sys, duration=0.2e-3)
+area_y = (np.arange(ny)-ny/2.0)*deltak[1]
+area_z = (np.arange(nz)-nz/2.0)*deltak[2]
 
 # Calculate timing
 TE = 10e-3
 TR = 40e-3
-delayTE = TE - mr.calcDuration(rf)/2.0 - mr.calcDuration(gxPre) - mr.calcDuration(gx)/2.0
-delayTR = TR - mr.calcDuration(rf)     - mr.calcDuration(gxPre) - mr.calcDuration(gx) - mr.calcDuration(gxSpoil) - delayTE
+delay_TE = TE - mr.calc_duration(rf)/2.0 - mr.calc_duration(gx_pre) -\
+    mr.calc_duration(gx)/2.0
+delay_TR = TR - mr.calc_duration(rf) - mr.calc_duration(gx_pre) -\
+    mr.calc_duration(gx) - mr.calc_duration(gx_spoil) - delay_TE
 
 # Drive magnetization to steady state
-for iY in range(1,Ndummy+1):
+for iY in range(1, n_dummy+1):
     # RF
     # Create non-selective pulse
-    rf = mr.makeBlockPulse(8*np.pi/180, system=sys, duration=0.2e-3, 
-            phaseOffset=(np.mod(117.0*(iY**2+iY+2)*np.pi/180,2*np.pi)))
-    seq.addBlock(rf)
+    rf = mr.make_block_pulse(8*np.pi/180, system=sys, duration=0.2e-3,
+                             phase_offset=(np.mod(117.0*(iY**2+iY+2)*np.pi/180,
+                                                  2*np.pi)))
+    seq.add_block(rf)
     # Gradients
-    gyPre  = mr.makeTrapezoid('y', system=sys, area= areaY[int(np.floor(Ny/2.0))-1], duration=Tpre)
-    gyReph = mr.makeTrapezoid('y', system=sys, area=-areaY[int(np.floor(Ny/2.0))-1], duration=Tpre)
-    #flupidu
-    seq.addBlock([gxPre, gyPre])
-    seq.addBlock(mr.makeDelay(delayTE))
-    seq.addBlock(gx)
-    seq.addBlock([gyReph, gxSpoil])
-    seq.addBlock(mr.makeDelay(delayTR))
+    gy_pre = mr.make_trapezoid('y', system=sys,
+                               area=area_y[int(np.floor(ny/2.0))-1],
+                               duration=t_pre)
+    gy_reph = mr.make_trapezoid('y', system=sys,
+                                area=-area_y[int(np.floor(ny/2.0))-1],
+                                duration=t_pre)
+
+    seq.add_block([gx_pre, gy_pre])
+    seq.add_block(mr.make_delay(delay_TE))
+    seq.add_block(gx)
+    seq.add_block([gy_reph, gx_spoil])
+    seq.add_block(mr.make_delay(delay_TR))
 
 # Make trapezoids for inner loop to save computation
-gyPre  = []
-gyReph = []
-rf     = []
-adc    = []
-for iY in range(1,Ny+1):
-    gyPre.append( mr.makeTrapezoid('y', system=sys, area= areaY[iY-1], duration=Tpre))
-    gyReph.append(mr.makeTrapezoid('y', system=sys, area=-areaY[iY-1], duration=Tpre))
-    phaseOffset = np.mod(117.0*(iY**2+iY+2.0)*np.pi/180.0,2*np.pi)
-    rf.append(mr.makeBlockPulse(8*np.pi/180.0, system=sys, duration=0.2e-3, phaseOffset=phaseOffset))
-    adc.append(mr.makeAdc(Nx, system=sys, duration=gx.flatTime, delay=gx.riseTime, phaseOffset=phaseOffset))
+gy_pre = []
+gy_reph = []
+rf = []
+adc = []
+for iY in range(1, ny+1):
+    gy_pre.append(mr.make_trapezoid('y', system=sys, area=area_y[iY-1],
+                                    duration=t_pre))
+    gy_reph.append(mr.make_trapezoid('y', system=sys, area=-area_y[iY-1],
+                                     duration=t_pre))
+    phase_offset = np.mod(117.0*(iY**2+iY+2.0)*np.pi/180.0, 2*np.pi)
+    rf.append(mr.make_block_pulse(8*np.pi/180.0, system=sys, duration=0.2e-3,
+                                  phase_offset=phase_offset))
+    adc.append(mr.make_adc(nx, system=sys, duration=gx.flat_time,
+                           delay=gx.rise_time, phase_offset=phase_offset))
 
 
 # Loop over phase encodes and define sequence blocks
-for iZ in range(Nz):
-    gzPre  = mr.makeTrapezoid('z', system=sys, area= areaZ[iZ], duration=Tpre)
-    gzReph = mr.makeTrapezoid('z', system=sys, area=-areaZ[iZ], duration=Tpre)
-    for iY in range(Ny):
+for iZ in range(nz):
+    gz_pre = mr.make_trapezoid('z', system=sys, area=area_z[iZ],
+                               duration=t_pre)
+    gz_reph = mr.make_trapezoid('z', system=sys, area=-area_z[iZ],
+                                duration=t_pre)
+    for iY in range(ny):
         # Excitation with RF spoiling
-        seq.addBlock(rf[iY])
+        seq.add_block(rf[iY])
 
         # Encoding
-        seq.addBlock([gxPre, gyPre[iY], gzPre])
-        seq.addBlock(mr.makeDelay(delayTE))
-        seq.addBlock([gx, adc[iY]])
-        seq.addBlock([gyReph[iY], gzReph, gxSpoil])
-        seq.addBlock(mr.makeDelay(delayTR))
+        seq.add_block([gx_pre, gy_pre[iY], gz_pre])
+        seq.add_block(mr.make_delay(delay_TE))
+        seq.add_block([gx, adc[iY]])
+        seq.add_block([gy_reph[iY], gz_reph, gx_spoil])
+        seq.add_block(mr.make_delay(delay_TR))
 
 # Output in Pulseq format for execution
 seq.write('external.seq')
